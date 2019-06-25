54a79b766486d0dff229e608ddc5eeed6288748f
Anthony Munoz <anthony.munozm@gmail.com>
2019-06-25 15:53:27 -0500

    adding fixed version

--- models/models.py
+++ models/models.py
@@ -1,69 +1,74 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, TEXT, Date
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
 
-class ShortForm(Model):
-    id = Column(Integer, primary_key=True)
-    price = Column(Integer(80), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    last_4_of_ssn = Column(Integer(80), unique=True, nullable=False)
-    birthday = Column(db.Date)
-    zip = Column(Integer(10), unique=True, nullable=False)
+
+class ShortForm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    last_4_of_ssn = db.Column(db.Integer, unique=True, nullable=False)
+    birthday = db.Column(db.Date)
+    zip = db.Column(db.Integer, unique=True, nullable=False)
 
     def __repr__(self):
         return self . username
 
 
 class LongFormStep1(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(TEXT, unique=True, nullable=False)
+    last_name = db.Column(TEXT, unique=True, nullable=False)
+    street_adress = db.Column(String(120), unique=True, nullable=False)
+    code_city = db.Column(String(2), unique=True, nullable=False)
+    city = db.Column(String(40), unique=True, nullable=False)
+    zip = db.Column(Integer, unique=True, nullable=False)
+    phone = db.Column(Integer, unique=True, nullable=False)
+    mobil_phone = db.Column(Integer, unique=True, nullable=False)
+    email = db.Column(String(120), unique=True, nullable=False)
+    birthday = db.Column(Date)
+    social_security = db.Column(Integer, unique=True, nullable=False)
+    driver_license = db.Column(Integer, unique=True, nullable=False)
+    issuing_state = db.Column(String(80), unique=True, nullable=False)
+    home_ownership = db.Column(String(80), unique=True, nullable=False)
+    time_at_residence = db.Column(String(40), unique=True, nullable=False)
+
+    def __repr__(self):
+        return self . username
+
+
+class LongFormStep2(db.Model):
     id = db.Column(Integer, primary_key=True)
-    first_name = Column(String(120), unique=True, nullable=False)
-    last_name = Column(String(120), unique=True, nullable=False)
-    street_adress = Column(String(120), unique=True, nullable=False)
-    code_city = Column(String(2), unique=True, nullable=False)
-    city = Column(String(40), unique=True, nullable=False)
-    zip = Column(Integer(10), unique=True, nullable=False)
-    phone = Column(Integer(20), unique=True, nullable=False)
-    mobil_phone = Column(Integer(20), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    birthday = Column(Date)
-    social_security = Column(Integer(80), unique=True, nullable=False)
-    driver_license = Column(Integer(80), unique=True, nullable=False)
-    issuing_state = Column(String(80), unique=True, nullable=False)
-    home_ownership = Column(String(80), unique=True, nullable=False)
-    time_at_residence = Column(String(40), unique=True, nullable=False)
+    military = db.Column(db.String(10), unique=True, nullable=False)
+    income = db.Column(String(10), unique=True, nullable=False)
+    company_name = db.Column(String(80), unique=True, nullable=False)
+    position = db.Column(String(80), unique=True, nullable=False)
+    work_phone = db.Column(Integer, unique=True, nullable=False)
+    time_of_job = db.Column(String(10), unique=True, nullable=False)
+    total_monthly_income = db.Column(String(80), unique=True, nullable=False)
+    payment_frequency = db.Column(String(20), unique=True, nullable=False)
+    first_payment = db.Column(Date)
+    second_payment = db.Column(Date)
+    deposit = db.Column(String(10), unique=True, nullable=False)
 
     def __repr__(self):
         return self . username
 
-
-class LongFormStep2(Model):
-    id = Column(Integer, primary_key=True)
-    military = Column(String(10), unique=True, nullable=False)
-    income = Column(String(10), unique=True, nullable=False)
-    company_name = Column(String(80), unique=True, nullable=False)
-    position = Column(String(80), unique=True, nullable=False)
-    work_phone = Column(Integer(20), unique=True, nullable=False)
-    time_of_job = Column(String(10), unique=True, nullable=False)
-    total_monthly_income = Column(String(80), unique=True, nullable=False)
-    payment_frequency = Column(String(20), unique=True, nullable=False)
-    first_payment = Column(Date)
-    second_payment = Column(Date)
-    deposit = Column(String(10), unique=True, nullable=False)
-
-    def __repr__(self):
-        return self . username
-
-class LongFormStep3(Model):
-    id = Column(Integer, primary_key=True)
-    aba = Column(Integer(10), unique=True, nullable=False)
-    bank_name = Column(String(10), unique=True, nullable=False)
-    time_at_bank = Column(String(10), unique=True, nullable=False)
-    checking = Column(String(10), unique=True, nullable=False)
-    checking_account = Column(Integer(80), unique=True, nullable=False)
+class LongFormStep3(db.Model):
+    id = db.Column(Integer, primary_key=True)
+    aba = db.Column(Integer, unique=True, nullable=False)
+    bank_name = db.Column(String(10), unique=True, nullable=False)
+    time_at_bank = db.Column(String(10), unique=True, nullable=False)
+    checking = db.Column(String(10), unique=True, nullable=False)
+    checking_account = db.Column(Integer, unique=True, nullable=False)
 
     def __repr__(self):
         return self . username
